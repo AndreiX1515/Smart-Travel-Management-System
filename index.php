b2e030821f237a8317685f26f0a17930143c107f
AndreiX1515 <dreideguzman333@gmail.com>
2024-09-26 09:50:58 +0800

    Add Navbar Option

--- index.php
+++ index.php
@@ -47,6 +47,7 @@
             <a href="#" class="nav-link">Tours</a>
             <a href="#" class="nav-link">About</a>
             <a href="#" class="nav-link">Contact</a>
+            <a href="#" class="nav-link">Contact</a>
         </nav>
 
         <button class="btn btn-primary" id="LoginButton">Book Now</button>
