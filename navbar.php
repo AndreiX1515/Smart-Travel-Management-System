092019e1f8b730321e7ebb00c7cc536dbb2591a0
AndreiX1515 <dreideguzman333@gmail.com>
2024-10-08 17:06:16 +0800

    added contents in the homepage

---
+++ navbar.php
@@ -0,0 +1,52 @@
+<!-- Main Header -->
+<header class="header d-flex justify-content-between align-items-center">
+        <div class="logo">
+            <a href="#" class="logo"><img src="assets\images\SMART LOGO 2 (2).png" alt="Logo"></a>
+        </div>
+        
+        <nav class="navbar d-flex justify-content-end">
+          <a href="#" class="nav-link">Home</a>
+
+        <!-- Packages Dropdown -->
+        <div class="dropdown">
+            <a href="#" class="nav-link dropdown-toggle" id="packagesDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                Packages
+            </a>
+            <ul class="dropdown-menu" aria-labelledby="packagesDropdown">
+                <li><a class="dropdown-subitem" href="#">Winter</a></li>
+                <li><a class="dropdown-subitem" href="#">Spring</a></li>
+                <li><a class="dropdown-subitem" href="#">Summer</a></li>
+                <li><a class="dropdown-subitem" href="#">Autumn</a></li>
+                <li><a class="dropdown-subitem" href="#">Seoul</a></li>
+                <li><a class="dropdown-subitem" href="#">Busan</a></li>
+            </ul>
+        </div>
+
+      <div class="dropdown">
+          <a href="#" class="nav-link dropdown-toggle" id="hotelDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+              Hotel
+          </a>
+          <ul class="dropdown-menu" aria-labelledby="hotelDropdown">
+              <li><a class="dropdown-subitem" href="#">Airsky Hotel</a></li>
+              <li><a class="dropdown-subitem" href="#">Recenz Hotel</a></li>
+              <li><a class="dropdown-subitem" href="#">Insadong Ibis</a></li>
+              <li><a class="dropdown-subitem" href="#">Marina Bay</a></li>
+          </ul>
+      </div>
+
+          <a href="#" class="nav-link">About</a>
+          <a href="#" class="nav-link">Contact</a>
+
+    <!-- <div class="dropdown">
+        <a href="#" class="nav-link dropdown-toggle" id="othersDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Others
+        </a>
+        <ul class="dropdown-menu" aria-labelledby="othersDropdown">
+            <li><a class="dropdown-subitem" href="#">Visa Requirements</a></li>
+        </ul>
+    </div> -->
+
+    <div class="login-container ms-5">
+        <button class="btn btn-primary text-lighter fw-bold" id="LoginButton">Book Now</button>
+    </div>
+</nav>
