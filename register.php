b3619d626231d0ac594ce0ce9ce5f4fb57b82e28
AndreiX1515 <dreideguzman333@gmail.com>
2024-10-14 09:06:37 +0800

    Send OTP Error

--- register.php
+++ register.php
@@ -202,12 +202,10 @@ error_reporting(E_ALL);
         });
 
 
-        let emailField = $('#floatingEmail');
-        let email = emailField.val();
-        let passwordField = $('#floatingPassword');
-        let password = passwordField.val();
-        let cpasswordField = $('#floatingPassword2');
-        let cpassword = cpasswordField.val();
+        
+
+
+
 
 
 
@@ -215,13 +213,13 @@ error_reporting(E_ALL);
         $('#SubmitRegButton').on('click', function (e) {
             e.preventDefault(); // Prevent default behavior
 
-            // // Retrieve input field values
-            // let emailField = $('#floatingEmail');
-            // let email = emailField.val();
-            // let passwordField = $('#floatingPassword');
-            // let password = passwordField.val();
-            // let cpasswordField = $('#floatingPassword2');
-            // let cpassword = cpasswordField.val();
+            // Retrieve input field values
+            let emailField = $('#floatingEmail');
+            let email = emailField.val();
+            let passwordField = $('#floatingPassword');
+            let password = passwordField.val();
+            let cpasswordField = $('#floatingPassword2');
+            let cpassword = cpasswordField.val();
 
             // Clear any previous message and reset borders
             $('#message-1').text('').removeClass('show');
@@ -354,7 +352,13 @@ error_reporting(E_ALL);
             // Start the OTP countdown immediately when the link is clicked
             startOtpCountdown(this); // Pass the link element
 
-            
+            // Retrieve input field values
+            let emailField = $('#floatingEmail');
+            let email = emailField.val();
+            let passwordField = $('#floatingPassword');
+            let password = passwordField.val();
+            let cpasswordField = $('#floatingPassword2');
+            let cpassword = cpasswordField.val();
 
             $.ajax({
                 url: 'send-otp.php',
