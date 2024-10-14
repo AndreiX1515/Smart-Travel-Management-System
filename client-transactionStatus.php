4eb644d57da947bd430997c624939e461356473d
AndreiX1515 <dreideguzman333@gmail.com>
2024-10-14 11:00:17 +0800

    Added inquiry button

--- client-transactionStatus.php
+++ client-transactionStatus.php
@@ -41,7 +41,6 @@
       </ul>
     </nav>
             
-<<<<<<< HEAD
         <!-- Main content -->
         <div id="main-content" class="col-md-9 col-lg-10 w-100 p-5">
           <div class="table-responsive">
@@ -196,111 +195,6 @@
               </table>
           </div>
       </div>
-=======
-    <!-- Main content -->
-    <div id="main-content" class="col-md-9 col-lg-10 w-100 p-5">
-      <div class="table-responsive">
-        <div class="table-controls mb-4">
-          <div class="d-flex flex-column">
-            <label for="" class="mb-2">Search</label>
-            <input type="text" class="search-input" placeholder="Search..." />
-          </div>
-
-          <div class="d-flex flex-column">
-            <label for="" class="mb-2 ms-2">Package Name</label>
-            <select class="sort-dropdown">
-              <option value="">Sort by Package Name</option>
-              <option value="packageNameAsc">Package Name (A-Z)</option>
-              <option value="packageNameDesc">Package Name (Z-A)</option>
-            </select>
-          </div>
-
-          <div class="d-flex flex-column">
-            <label for="" class="mb-2 ms-2">Package Name</label>
-            <select class="sort-dropdown">
-              <option value="">Sort by Package Name</option>
-              <option value="packageNameAsc">Package Name (A-Z)</option>
-              <option value="packageNameDesc">Package Name (Z-A)</option>
-            </select>
-          </div>
-
-          <div class="d-flex flex-column">
-            <label for="" class="mb-2 ms-2">Status</label>
-            <select class="sort-dropdown">
-              <option value="">Sort by Status</option>
-              <option value="StatusCompleted">Completed</option>
-              <option value="StatusPending">Pending</option>
-              <option value="StatusOngoing">Ongoing</option>
-              <option value="StatusTerminated">Terminated</option>
-            </select>
-          </div>
-
-          <!-- <div class="d-flex flex-row align-items-end">
-            <button class="add-button">Add Booking</button>
-          </div> -->
-        </div>
-        <table class="table excel-table">
-          <thead>
-            <tr>
-              <th scope="col">Transaction Number</th>
-              <th scope="col">Package Name</th>
-              <th scope="col">Flight Date</th>
-              <th scope="col">Total Pax</th>
-              <th scope="col">Amount To Pay</th>
-              <th scope="col">Downpayment Total</th>
-              <th scope="col">Status</th>
-              <th scope="col">Action</th>
-            </tr>
-          </thead>
-          <tbody>
-              <?php
-                $sql1 = "SELECT Distinct
-                          b.transactNo,
-                          b.pax AS totalPax,
-                          b.totalPrice AS amountToPay,
-                          b.downpaymentAmount AS downpayment, 
-                          b.status,
-                          p.packageName,
-                          DATE_FORMAT(f.flightDepartureDate, '%M %d, %Y') AS flightDate
-                            FROM 
-                                booking b
-                            JOIN 
-                                guest g ON b.transactNo = g.transactNo
-                            JOIN 
-                                flight f ON g.flightId = f.flightId
-                            JOIN 
-                                package p ON f.packageId = p.packageId
-                            WHERE 
-                                b.accountId = '$accId'";
-
-                $res1 = $conn->query($sql1);
-
-                if ($res1->num_rows > 0) 
-                {
-                  while ($row = $res1->fetch_assoc()) 
-                  {
-                    echo "<tr>
-                            <td>" . htmlspecialchars($row['transactNo']) . "</td>
-                            <td>" . htmlspecialchars($row['packageName']) . "</td>
-                            <td>" . htmlspecialchars($row['flightDate']) . "</td>
-                            <td>" . htmlspecialchars($row['totalPax']) . "</td>
-                            <td>₱ " . number_format($row['amountToPay'], 2) . "</td>
-                            <td>₱ " . number_format($row['downpayment'], 2) . "</td>
-                            <td>". $row['status'] ."</td>
-                            <td><button type='button' class='btn btn-info'>Inquiry</button></td>
-                          </tr>";
-                  }
-                } 
-                else 
-                {
-                  echo "<tr><td colspan='8'>No bookings found</td></tr>";
-                }
-              ?>
-          </tbody>
-        </table>
-      </div>
-    </div>
->>>>>>> 9b01304856b7703a48a9c70c5cb00dc655df30bc
   </div>
   
   
