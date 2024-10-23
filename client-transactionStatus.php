bf4087d99a7fd76360e94a593eaa06c5ec3221d0
Vlar.sanity <cordova.clarence09@gmail.com>
2024-10-23 17:04:42 +0800

    added back button in client transaction history page

--- client-transactionStatus.php
+++ client-transactionStatus.php
@@ -28,6 +28,8 @@
       endif;
     ?>
 
+    <a class="btn btn-primary me-2" href="client-dashboard.php" role="button">Go Back</a>
+
       <div class="table-responsive px-4">
         <div class="table-controls mb-4">
           <div class="d-flex flex-column">
@@ -93,7 +95,7 @@
                     b.transactNo,
                     b.pax AS totalPax,
                     b.totalPrice AS amountToPay,
-                    b.downpaymentAmount as downpayment,
+                    a.amount as downpayment,
                     b.status, b.agentId, 
                     p.packageName,
                     DATE_FORMAT(f.flightDepartureDate, '%M %d, %Y') AS flightDate
@@ -105,6 +107,8 @@
                     flight f ON g.flightId = f.flightId
                 JOIN 
                     package p ON f.packageId = p.packageId
+                JOIN 
+                    payment a ON a.transactNo = b.transactNo
                 WHERE 
                     b.accountId = '$accId'";
 
@@ -285,7 +289,7 @@
                                              <div class='mb-3'>
                                                  <label class='form-label fw-bold'>Payment History</label>
                                                 
-
+                                                
 
 
                                              </div>
