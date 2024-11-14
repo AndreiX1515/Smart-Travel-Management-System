2b1d8df0c02b147c49b017e262d365f5b8876a0c
Vlar.sanity <cordova.clarence09@gmail.com>
2024-11-14 13:05:20 +0800

    soa sample computation

--- Agent Section/agent-transactions.php
+++ Agent Section/agent-transactions.php
@@ -130,7 +130,7 @@
               LEFT JOIN
                   agent a ON b.agentId = a.agentId
               WHERE 
-                  b.agentId = '$agentId' AND b.status = 'Pending'
+                  b.agentId = '$agentId' 
               ORDER BY 
                   b.transactNo DESC LIMIT 10";
 
@@ -147,10 +147,10 @@
                   $statusClass = '';
 
                   switch ($status) {
-                      case 'Active':
+                      case 'Confirmed':
                           $statusClass = 'bg-success text-white'; // Green background, white text
                           break;
-                      case 'Inactive':
+                      case 'Cancelled':
                           $statusClass = 'bg-danger text-white'; // Red background, white text
                           break;
                       case 'Pending':
