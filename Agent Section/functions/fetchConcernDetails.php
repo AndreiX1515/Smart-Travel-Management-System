7927334b17f55b9e8646641143b9a3d8b5354f9e
Vlar.sanity <cordova.clarence09@gmail.com>
2024-11-21 17:40:25 +0800

    fixed query for concern

--- Agent Section/functions/fetchConcernDetails.php
+++ Agent Section/functions/fetchConcernDetails.php
@@ -6,7 +6,7 @@
     $concernId = $_POST['concernId'];
 
     // Fetch distinct details and their prices based on the selected concernId
-    $sql = mysqli_query($conn, "SELECT concernDetailsId, details, price FROM concernDetails WHERE concernId = '$concernId' ORDER BY details ASC");
+    $sql = mysqli_query($conn, "SELECT concernDetailsId, details, price FROM concerndetails WHERE concernId = '$concernId' ORDER BY details ASC");
 
     $detailsOptions = '<option selected disabled>Select Detail</option>'; // Default option
 
