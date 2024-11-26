f9f9c09520cd9bc31849e6b99c34d7b3295b78a4
AndreiX1515 <dreideguzman333@gmail.com>
2024-11-26 17:17:42 +0800

    Adjustments on Visa Requirements Table

--- Agent Section/agent-showVisa.php
+++ Agent Section/agent-showVisa.php
@@ -51,9 +51,21 @@
                            <a href='functions/view-file.php?file=" . urlencode($row['passport']) . "' target='_blank'>View File</a> 
                            <a href='functions/download.php?file=" . urlencode($row['passport']) . "' target='_blank'>Download File</a> 
                          </td>
-                         <td>{$row['permit']}</td>
-                         <td>{$row['validId']}</td>
-                         <td>{$row['certificate']}</td>
+
+                         <td>
+                           <a href='functions/view-file.php?file=" . urlencode($row['permit']) . "' target='_blank'>View File</a> 
+                           <a href='functions/download.php?file=" . urlencode($row['permit']) . "' target='_blank'>Download File</a> 
+                         </td>
+
+                         <td>
+                           <a href='functions/view-file.php?file=" . urlencode($row['validId']) . "' target='_blank'>View File</a> 
+                           <a href='functions/download.php?file=" . urlencode($row['validId']) . "' target='_blank'>Download File</a> 
+                         </td>
+
+                         <td>
+                           <a href='functions/view-file.php?file=" . urlencode($row['certificate']) . "' target='_blank'>View File</a> 
+                           <a href='functions/download.php?file=" . urlencode($row['certificate']) . "' target='_blank'>Download File</a> 
+                         </td>
                        </tr>";
                }
              } else {
