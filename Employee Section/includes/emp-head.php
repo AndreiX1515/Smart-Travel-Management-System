31b836cca788387cda71fcaad1f54dbbf8b21546
AndreiX1515 <dreideguzman333@gmail.com>
2024-11-28 16:18:20 +0800

    Emp Logout

--- Employee Section/includes/emp-head.php
+++ Employee Section/includes/emp-head.php
@@ -1,8 +1,10 @@
 <!-- Bootstrap 5.3 CSS -->
-<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
+<link href="https://unpkg.com/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+
 
 <!-- Font Awesome 6.6 CSS -->
-<link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css" rel="stylesheet"> <!-- Font Awesome 6.6 -->
+<link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css" rel="stylesheet">
+
 
 <!-- DataTables CSS -->
 <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/datatables.net-dt/css/jquery.dataTables.min.css">
