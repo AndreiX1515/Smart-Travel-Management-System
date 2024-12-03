264fbfbce097858189c0395accf4e27828d22944
Vlar.sanity <cordova.clarence09@gmail.com>
2024-12-03 16:28:20 +0800

    working for audit

--- Agent Section/functions/agent-addBooking-code.php
+++ Agent Section/functions/agent-addBooking-code.php
@@ -43,6 +43,9 @@
       $flightId = NULL; // Set flightId to NULL if "Own Flight" is selected
     }
 
+    // Set the session variable for the current user in MySQL
+    $conn->query("SET @current_user_id = $accountId");
+
     // Start a transaction
     $conn->begin_transaction();
 
