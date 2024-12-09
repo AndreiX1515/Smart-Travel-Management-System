659023ef79409c087ea164efe7fe7c81b92c896b
Vlar.sanity <cordova.clarence09@gmail.com>
2024-12-09 13:50:27 +0800

    adjsuted title for dashboard, ang guest table adjust visa modal

--- Agent Section/agent-dashboard.php
+++ Agent Section/agent-dashboard.php
@@ -302,7 +302,7 @@
         <!-- CARD 3 - Total Payment -->
         <div class="card border-0" >
           <div class="header-counts">
-            <h6 class="text-secondary fw-600 white-pill">Total Payment</h6>
+            <h6 class="text-secondary fw-600 white-pill">Total Sales</h6>
           </div>
 
           <div class="card-content px-3">
