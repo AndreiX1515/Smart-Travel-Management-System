c8b5a1d28e666cb51beade54374aece5094ec5d5
Vlar.sanity <cordova.clarence09@gmail.com>
2024-12-12 16:15:12 +0800

    fixed adjustments based on hostinger

--- Employee Section/emp-transaction.php
+++ Employee Section/emp-transaction.php
@@ -145,7 +145,7 @@
           // SQL query for SOA
           $sql = "SELECT b.transactNo, f.flightDepartureDate as departureDate, f.returnDepartureDate as returnDate, b.status as bookingStatus,
                       CONCAT(f.flightDepartureDate, ' | ', f.returnDepartureDate) AS FlightDate, p.packageName AS PackageName, 
-                      b.bookingDate AS BookingDate, b.pax AS TotalPax, b.totalPrice AS PackagePrice, 
+                      DATE_FORMAT(b.bookingDate, '%m.%d.%Y') AS BookingDate, b.pax AS TotalPax, b.totalPrice AS PackagePrice, 
                       CONCAT(a.lName, ', ', a.fName, ' ', IFNULL(CONCAT(SUBSTRING(a.mName, 1, 1), '.'), '')) AS agentName
                   FROM 
                     booking b
