3195170b45376ae0936acb77d3ba3d71d2348bef
AndreiX1515 <dreideguzman333@gmail.com>
2025-01-16 11:42:21 +0800

    Design adjustments Agent Transactions

--- Agent Section/agent-dashboard copy 2.php
+++ Agent Section/agent-dashboard copy 2.php
@@ -26,9 +26,68 @@ session_start();
     </div>
 
     <div class="main-content">
+      <div class="count-containers">
+        <div class="count-card">
+          <div class="card-header">
+            <div class="card-name">
+              <h5>Transactions</h5>
+            </div>
+            
+            <div class="pill-container">
+              <ul class="nav nav-pills" id="pills-tab" role="tablist">
+                <li class="nav-item" role="presentation">
+                  <button class="nav-link active" id="pills-home-tab" data-bs-toggle="pill" data-bs-target="#pills-home" type="button" role="tab" aria-controls="pills-home" aria-selected="true">Current & Pending</button>
+                </li>
+                <li class="nav-item" role="presentation">
+                  <button class="nav-link" id="pills-profile-tab" data-bs-toggle="pill" data-bs-target="#pills-profile" type="button" role="tab" aria-controls="pills-profile" aria-selected="false">Completed</button>
+                </li>
+                
+
+                <li class="nav-item" role="presentation">
+                  <button class="nav-link" id="pills-contact-tab" data-bs-toggle="pill" data-bs-target="#pills-contact" type="button" role="tab" aria-controls="pills-contact" aria-selected="false">Cancelled</button>
+                </li>
+              </ul>
+            </div>
+
+          </div>
+
+          <div class="card-body">
+            <div class="tab-content" id="pills-tabContent">
+              <div class="tab-pane fade show active" id="pills-home" role="tabpanel" aria-labelledby="pills-home-tab" tabindex="0">
+
+              </div>
+
+              <div class="tab-pane fade" id="pills-profile" role="tabpanel" aria-labelledby="pills-profile-tab" tabindex="0">
+
+              </div>
+
+              <div class="tab-pane fade" id="pills-contact" role="tabpanel" aria-labelledby="pills-contact-tab" tabindex="0">
+
+
+              </div>
+
+              <div class="tab-pane fade" id="pills-disabled" role="tabpanel" aria-labelledby="pills-disabled-tab" tabindex="0">
+
+
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div class="count-card">
+          
+        </div>
+        <div class="count-card">
+          
+        </div>
+        <div class="count-card">
+          
+        </div>
+      </div>
 
     </div>
   </div>
+
 
 
 
