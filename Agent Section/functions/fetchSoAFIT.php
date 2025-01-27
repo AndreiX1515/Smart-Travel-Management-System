9bad8ca643156470ede91728ce2052c8fabaa579
Vlar.sanity <cordova.clarence09@gmail.com>
2025-01-27 11:05:23 +0800

    function/ SoAFIT

--- Agent Section/functions/fetchSoAFIT.php
+++ Agent Section/functions/fetchSoAFIT.php
@@ -4,20 +4,24 @@ require "../../conn.php"; // Include the DB connection
 session_start();
 
 // Get the selected filter values from the POST request
-$month = isset($_POST['month']) ? date('m', strtotime($_POST['month'])) : date('m');
-$year = isset($_POST['year']) ? $_POST['year'] : date('Y');
+$month = isset($_POST['month']) ? intval(date('m', strtotime($_POST['month']))) : date('m');
+$year = isset($_POST['year']) ? intval($_POST['year']) : date('Y');
 
-// Initialize variables
+// Initialize totals and count variables
+$totalPhpSum = 0;
+$totalUsdSum = 0;
+$totalPaymentAmount = 0;
+$balance = 0;
+$count = 1;
+
+// Initialize data availability flag and table variables
 $dataAvailable = false;
 $table1 = '';
 $table2 = '';
-$totalPhpSum = 0;
-$totalUsdSum = 0;
-$count = 1;
 
 // Query: Fetch booking data
-$sql = "SELECT f.startDate AS startDate, fh.hotelName AS hotelName, fr.rooms AS roomType, f.rooms AS NoofRooms, f.pax AS pax, 
-          f.phpPrice AS bookingPhpPrice, f.usdPrice AS bookingUsdPrice
+$sql = "SELECT f.startDate AS startDate, fh.hotelName AS hotelName, fr.rooms AS roomType, fr.price as roomPrice, f.rooms AS NoofRooms, 
+          f.pax AS pax, f.phpPrice AS bookingPhpPrice, f.usdPrice AS bookingUsdPrice
         FROM `fit` f
         JOIN fithotel fh ON f.hotelId = fh.hotelId
         JOIN fitrooms fr ON f.roomId = fr.roomId
@@ -31,21 +35,18 @@ if ($res && $res->num_rows > 0)
   while ($row = $res->fetch_assoc()) 
   {
     // Booking Details
-    $formattedBookingPhpPrice = number_format($row['bookingPhpPrice'], 2);
-    $formattedBookingUsdPrice = number_format($row['bookingUsdPrice'], 2);
-    $bookingPhpTotal = $row['bookingPhpPrice'] * $row['pax'];
-    $bookingUsdTotal = $row['bookingUsdPrice'] * $row['pax'];
-    $totalPhpSum += $bookingPhpTotal;
-    $totalUsdSum += $bookingUsdTotal;
+    $formattedBookingUsdPrice = number_format($row['roomPrice'], 2);
+    $totalPhpSum += $row['bookingPhpPrice'];
+    $totalUsdSum += $row['bookingUsdPrice'];
 
     $table1 .= "<tr>
                   <td>$count</td>
                   <td>{$row['hotelName']} ({$row['roomType']}) No. of Rooms: {$row['NoofRooms']}</td>
                   <td>$ $formattedBookingUsdPrice</td>
-                  <td>₱ $formattedBookingPhpPrice</td>
+                  <td>-</td>
                   <td>{$row['pax']}</td>
-                  <td>$ " . number_format($bookingUsdTotal, 2) . "</td>
-                  <td>₱ " . number_format($bookingPhpTotal, 2) . "</td>
+                  <td>$ " . number_format($row['bookingUsdPrice'], 2) . "</td>
+                  <td>₱ " . number_format($row['bookingPhpPrice'], 2) . "</td>
                 </tr>";
     $count++;
   }
@@ -55,12 +56,12 @@ else
   $table1 = "<tr><td colspan='7'>No Record found</td></tr>";
 }
 
+// Query: Fetch payment data
 $sql2 = "SELECT fp.paymentType AS paymentType,  DATE_FORMAT(fp.paymentDate, '%M %d, %Y') AS paymentDate, fp.amount AS paymentAmount 
          FROM `fit` f
          JOIN fitpayment fp ON f.transactionNo = fp.transactNo
          WHERE MONTH(f.startDate) = $month AND YEAR(f.startDate) = $year AND f.status = 'Completed' AND fp.paymentStatus = 'Approved'";
 
-// Execute the query for payment data
 $res2 = $conn->query($sql2);
 
 if ($res2 && $res2->num_rows > 0) 
@@ -70,6 +71,7 @@ if ($res2 && $res2->num_rows > 0)
   {
     // Payment Details
     $formattedPaymentAmount = number_format($row['paymentAmount'], 2);
+    $totalPaymentAmount += $row['paymentAmount'];
 
     $table2 .= "<tr>
                   <td>$count</td>
@@ -91,6 +93,9 @@ else
 // Generate subtotals
 $formattedTotalPhpSum = number_format($totalPhpSum, 2);
 $formattedTotalUsdSum = number_format($totalUsdSum, 2);
+$formattedTotalPaymentAmount = number_format($totalPaymentAmount, 2);
+$balance = $totalPhpSum - $totalPaymentAmount;
+$formattedBalance = number_format($balance, 2);
 
 // Generate combined response
 $response = "
@@ -128,8 +133,32 @@ $response = "
       $table2
     </tbody>
   </table>
-";
-
+  <div class='subtotal-container'>
+    <div class='balance'>
+      <span>SUBTOTAL: </span>
+    </div>
+    <div class='subtotal-item-usd'>
+      <span>USD:</span>
+      <span class='subtotal-usd'></span>
+    </div>
+    <div class='subtotal-item-php'>
+      <span>PHP:</span>
+      <span class='subtotal-php'>₱ $formattedTotalPaymentAmount</span>
+    </div>
+  </div>
+  <div class='balance-container'>
+    <div class='balance'>
+      <span>BALANCE:</span>
+    </div>
+    <div class='balanceUSD'>
+      <span>USD:</span>
+      <span class='subtotal-usd'></span>
+    </div>
+    <div class='balancePHP'>
+      <span>PHP:</span>
+      <span class='subtotal-php'>₱ $formattedBalance</span>
+    </div>
+  </div>";
 
 // Combine response and data availability flag
 $responseData = [
