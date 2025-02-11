f6524ab55e6d5ba5ec4a64e20a85c8c8911cfbe0
Vlar.sanity <cordova.clarence09@gmail.com>
2025-02-11 11:16:08 +0800

    adjusted

--- Employee Section/includes/emp-sidebar.php
+++ Employee Section/includes/emp-sidebar.php
@@ -73,12 +73,12 @@ $position = htmlspecialchars(strtoupper($empId));
 			</a>
 		</li>
 
-		<li class="nav-item transaction">
+		<!-- <li class="nav-item transaction">
 			<a class="nav-link page-button" href="../Employee Section/emp-guestList.php" data-page-name="Guest List">
 				<div class="icon"><i class="fas fa-user"></i></div>
 				<span class="label">Guest List</span>
 			</a>
-		</li>
+		</li> -->
 
 		<li class="nav-item transaction">
 			<a class="nav-link page-button" href="../Employee Section/emp-requestList.php" data-page-name="Guest List">
