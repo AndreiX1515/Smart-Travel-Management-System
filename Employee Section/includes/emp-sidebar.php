7b9a212db1ab93481de93c229a9b517a6b26e4d3
AndreiX1515 <dreideguzman333@gmail.com>
2025-02-12 13:49:00 +0800

    Admin adjustments

--- Employee Section/includes/emp-sidebar.php
+++ Employee Section/includes/emp-sidebar.php
@@ -227,6 +227,33 @@ $position = htmlspecialchars(strtoupper($empId));
 
 </script>
 
+<script>
+  document.addEventListener('DOMContentLoaded', () => {
+    // Check if there's a saved title in local storage
+    const savedTitle = localStorage.getItem('pageTitle');
+    if (savedTitle) {
+        document.getElementById('page-title').textContent = savedTitle;
+    }
+
+    const buttons = document.querySelectorAll('.page-button');
+    buttons.forEach(button => {
+        button.addEventListener('click', (event) => {
+            event.preventDefault();
+            const newPageName = button.getAttribute('data-page-name');
+            document.getElementById('page-title').textContent = newPageName;
+
+            // Save the title to local storage
+            localStorage.setItem('pageTitle', newPageName);
+
+            const newUrl = button.getAttribute('href');
+            setTimeout(() => {
+                window.location.href = newUrl;
+            }, 25);
+        });
+    });
+  });
+</script>
+
 
 <script>
 	document.addEventListener("DOMContentLoaded", function() {
