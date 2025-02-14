33b7b52f00efa435d56f82fa0e597e181e1fb681
AndreiX1515 <dreideguzman333@gmail.com>
2025-02-14 16:30:03 +0800

    agent flight direct booking

--- Agent Section/agent-dashboard.php
+++ Agent Section/agent-dashboard.php
@@ -709,7 +709,7 @@ require "../conn.php";
                               echo '<td class="fw-bold">' . $row['AvailSeats'] . '</td>';
                               echo '<td class="fw-bolder">' . $row['AdditionalSeats'] . '</td>';
                               echo '<td>₱ ' . number_format($row['RetailPrice'], 2) . '</td>';
-                              echo '<td><a href="../Agent Section/agent-revisedAddBooking.php?flightid=' . urlencode($row['flightId']) . '" class="btn btn-primary">Book Now</a></td>';
+                              echo '<td><a href="../Agent Section/agent-revisedAddBooking-2.php?flightid=' . urlencode($row['flightId']) . '" class="btn btn-primary">Book Now</a></td>';
                               // echo '<td class="fw-bolder">' . $row['Air+Land'] . '</td>';
                               // echo '<td class="fw-bolder">' . $row['LandOnly'] . '</td>';
                               // echo '<td>₱ ' . number_format($row['WholesalePrice'], 2) . '</td>';
