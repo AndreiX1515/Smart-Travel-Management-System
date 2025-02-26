ceb83cde23114089414ac528a3c72a98f8fceb56
AndreiX1515 <dreideguzman333@gmail.com>
2025-02-26 17:13:54 +0800

    alert test

--- Global Assets/toast/script/toast.php
+++ Global Assets/toast/script/toast.php
@@ -1,6 +1,11 @@
+
+
+
 <!-- Global Bootstrap Toast -->
 <div aria-live="polite" aria-atomic="true" class="position-relative">
     <div class="toast-container top-0 end-0 p-3" id="toastContainer">
         <!-- Toasts will be dynamically added here -->
     </div>
 </div>
+
+<script></script>
