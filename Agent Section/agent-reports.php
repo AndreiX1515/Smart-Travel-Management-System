c69ec8f4c10dc99b951e6da627776deed3777038
Vlar.sanity <cordova.clarence09@gmail.com>
2025-04-25 10:34:46 +0800

    function/ agent report adjusted

--- Agent Section/agent-reports.php
+++ Agent Section/agent-reports.php
@@ -30,105 +30,166 @@ error_reporting(E_ALL);
       <div class="main-content">
         <div class="content-wrapper">
           <div class="content-body">
-
-          <form method="POST" id="reportForm">
-            <!-- Report Type Radio Button -->
-            <div class="mb-4">
-              <label class="form-label">Report Type:</label>
-              <div class="form-check form-check-inline">
-                <input class="form-check-input" type="radio" name="reportType" id="flightReport" value="flight" checked>
-                <label class="form-check-label" for="flightReport">Flight</label>
+            <form method="POST" id="reportForm">
+              <!-- Report Type Radio Button -->
+              <div class="mb-4">
+                <label class="form-label">Report Type:</label>
+                <div class="form-check form-check-inline">
+                  <input class="form-check-input" type="radio" name="reportType" id="flightReport" value="flight" checked>
+                  <label class="form-check-label" for="flightReport">Flight</label>
+                </div>
+                <div class="form-check form-check-inline">
+                  <input class="form-check-input" type="radio" name="reportType" id="monthlyReport" value="monthly">
+                  <label class="form-check-label" for="monthlyReport">Monthly</label>
+                </div>
+                <div class="form-check form-check-inline">
+                  <input class="form-check-input" type="radio" name="reportType" id="weeklyReport" value="weekly">
+                  <label class="form-check-label" for="weeklyReport">Weekly</label>
+                </div>
               </div>
-              <div class="form-check form-check-inline">
-                <input class="form-check-input" type="radio" name="reportType" id="monthlyReport" value="monthly" checked>
-                <label class="form-check-label" for="monthlyReport">Monthly</label>
-              </div>
-              <div class="form-check form-check-inline">
-                <input class="form-check-input" type="radio" name="reportType" id="weeklyReport" value="weekly">
-                <label class="form-check-label" for="weeklyReport">Weekly</label>
-              </div>
-            </div>
 
-            <!-- Flight Date Selector -->
-            <div id="flightSelector" class="mb-3" style="display: none;">
-              <label for="flightDate" class="form-label">Select Flight Date:</label>
-              <select class="form-select" name="flightDate" id="flightDate">
-                <option selected disabled>Select a flight date</option>
-                <?php
-                  // Assuming you have a database connection in $conn
-                  $query = "SELECT DISTINCT flightDepartureDate FROM flight ORDER BY flightDepartureDate ASC";
-                  $result = $conn->query($query);
+              <!-- Report For Type Radio Button -->
+              <div class="mb-4">
+                <label class="form-label">For:</label>
+                <div class="form-check form-check-inline">
+                  <input class="form-check-input" type="radio" name="reportFor" id="selfReport" value="self" checked>
+                  <label class="form-check-label" for="selfReport">Self</label>
+                </div>
+                <div class="form-check form-check-inline">
+                  <input class="form-check-input" type="radio" name="reportFor" id="agentReport" value="agent">
+                  <label class="form-check-label" for="agentReport">Agent</label>
+                </div>
+                <div class="form-check form-check-inline">
+                  <input class="form-check-input" type="radio" name="reportFor" id="clientReport" value="client">
+                  <label class="form-check-label" for="clientReport">Client</label>
+                </div>
+                <div class="form-check form-check-inline">
+                  <input class="form-check-input" type="radio" name="reportFor" id="allReport" value="all">
+                  <label class="form-check-label" for="allReport">All</label>
+                </div>
+              </div>
 
-                  if ($result->num_rows > 0) 
-                  {
-                    while ($row = $result->fetch_assoc()) 
+              <!-- Flight Date Selector -->
+              <div id="flightSelector" class="mb-3" style="display: none;">
+                <label for="flightDate" class="form-label">Select Flight Date:</label>
+                <select class="form-select" name="flightDate" id="flightDate">
+                  <option selected disabled>Select a flight date</option>
+                  <?php
+                    $query = "SELECT DISTINCT flightDepartureDate FROM flight ORDER BY flightDepartureDate ASC";
+                    $result = $conn->query($query);
+                    if ($result->num_rows > 0) 
                     {
-                      $date = $row['flightDepartureDate'];
-                      $formattedDate = date("M d, Y", strtotime($date));
-                      echo "<option value=\"$date\">$formattedDate</option>";
+                      while ($row = $result->fetch_assoc()) 
+                      {
+                        $date = $row['flightDepartureDate'];
+                        $formattedDate = date("M d, Y", strtotime($date));
+                        echo "<option value=\"$date\">$formattedDate</option>";
+                      }
+                    } 
+                    else 
+                    {
+                      echo "<option disabled>No flight dates available</option>";
                     }
-                  } 
-                  else 
-                  {
-                    echo "<option disabled>No flight dates available</option>";
-                  }
-                ?>
-              </select>
-            </div>
+                  ?>
+                </select>
+              </div>
 
+              <!-- Monthly Selector -->
+              <div id="monthlySelector" class="mb-3" style="display: none;">
+                <label for="month" class="form-label">Select Month:</label>
+                <select class="form-select" name="month" id="month">
+                  <option selected disabled>Select Month</option>
+                  <option>January</option>
+                  <option>February</option>
+                  <option>March</option>
+                  <option>April</option>
+                  <option>May</option>
+                  <option>June</option>
+                  <option>July</option>
+                  <option>August</option>
+                  <option>September</option>
+                  <option>October</option>
+                  <option>November</option>
+                  <option>December</option>
+                </select>
+              </div>
 
-            <!-- Monthly Selector -->
-            <div id="monthlySelector" class="mb-3" style="display: none;">
-              <label for="month" class="form-label">Select Month:</label>
-              <select class="form-select" name="month" id="month">
-                <option selected disabled>Select Month</option>
-                <option>January</option>
-                <option>February</option>
-                <option>March</option>
-                <option>April</option>
-                <option>May</option>
-                <option>June</option>
-                <option>July</option>
-                <option>August</option>
-                <option>September</option>
-                <option>October</option>
-                <option>November</option>
-                <option>December</option>
-              </select>
-            </div>
+              <!-- Weekly Selector -->
+              <div id="weeklySelector" class="mb-3" style="display: none;">
+                <label for="week" class="form-label">Select Week:</label>
+                <select class="form-select" id="week" name="week">
+                  <option selected disabled>Select a week</option>
+                </select>
+              </div>
 
-            <!-- Weekly Selector -->
-            <div id="weeklySelector" class="mb-3" style="display: none;">
-              <label for="week" class="form-label">Select Week:</label>
-              <select class="form-select" id="week" name="week">
-                <option selected disabled>Select a week</option>
-              </select>
-            </div>
+              <!-- Agent Selector -->
+              <div id="agentSelector" class="mb-3" style="display: none;">
+                <label for="agentSelect" class="form-label">Select Agent:</label>
+                <select class="form-select" name="selectedAgent" id="agentSelect">
+                  <option value="all">All Agents</option>
+                  <?php
+                    $agentQuery = "SELECT agentId, fName, mName, lName FROM agent WHERE agentCode = '$agentCode'";
+                    $agentResult = $conn->query($agentQuery);
 
-            <input name="agentCode" value="<?php echo $agentCode; ?>" value="Agent Code" hidden>
+                    if ($result->num_rows > 0) 
+                    {
+                      while ($row = $agentResult->fetch_assoc()) 
+                      {
+                        $fullName = $row['fName'] . ' ' . (!empty($row['mName']) ? substr($row['mName'], 0, 1) . '. ' : '') . $row['lName'];
+                        echo "<option value=\"{$row['agentId']}\">$fullName</option>";
+                      }
+                    } 
+                    else 
+                    {
+                      echo "<option disabled>No flight dates available</option>";
+                    }
 
-            <!-- Submit Button -->
-            <div class="content-footer">
-              <button type="submit" class="btn btn-primary">Generate Report</button>
-            </div>
-          </form>
+                  ?>
+                </select>
+              </div>
 
-          <!-- Table for Displaying Data -->
-          <table class="table" id="dataTable" style="display:none;">
-            <thead>
-              <tr>
-                <th>AGENT NAME</th>
-                <th>FLIGHT DATE</th>
-                <th>PAX</th>
-                <th>AMOUNT</th>
-              </tr>
-            </thead>
-            <tbody></tbody>
-          </table>
+              <!-- Client Selector -->
+              <div id="clientSelector" class="mb-3"  style="display: none;">
+                <label for="clientSelect" class="form-label">Select Client:</label>
+                <select class="form-select" name="selectedClient" id="clientSelect">
+                  <option value="all">All Clients</option>
+                  <?php
+                    $clientQuery = "SELECT clientId, fName, mName, lName FROM client WHERE clientCode = '$agentCode'";
+                    $clientResult = $conn->query($clientQuery);
 
-          <!-- Button to Generate the Report -->
-          <button id="downloadReport" class="btn btn-success" style="display: none;">Download Report</button>
+                    while ($row = $clientResult->fetch_assoc()) 
+                    {
+                      $fullName = $row['fName'] . ' ' . (!empty($row['mName']) ? substr($row['mName'], 0, 1) . '. ' : '') . $row['lName'];
+                      echo "<option value=\"{$row['clientId']}\">$fullName</option>";
+                    }
+                  ?>
+                </select>
+              </div>
 
+              <input name="agentCode" value="<?php echo $agentCode; ?>">
+              <input name="accountId" value="<?php echo $accountId; ?>">
+
+              <!-- Submit Button -->
+              <div class="content-footer">
+                <button type="submit" class="btn btn-primary">Generate Report</button>
+              </div>
+            </form>
+
+            <!-- Table for Displaying Data -->
+            <table class="table" id="dataTable" style="display:none;">
+              <thead>
+                <tr>
+                  <th>AGENT NAME</th>
+                  <th>FLIGHT DATE</th>
+                  <th>PAX</th>
+                  <th>AMOUNT</th>
+                </tr>
+              </thead>
+              <tbody></tbody>
+            </table>
+
+            <!-- Button to Generate the Report -->
+            <button id="downloadReport" class="btn btn-success" style="display: none;">Download Report</button>
           </div>
         </div>
       </div>
@@ -137,14 +198,29 @@ error_reporting(E_ALL);
 
   <!-- Optional Script to Toggle Selectors -->
   <script>
+    // Report Type Radio Buttons
     const flightRadio = document.getElementById('flightReport');
     const monthlyRadio = document.getElementById('monthlyReport');
     const weeklyRadio = document.getElementById('weeklyReport');
 
+    // Report For Radio Buttons
+    const selfReportRadio = document.getElementById('selfReport');
+    const agentReportRadio = document.getElementById('agentReport');
+    const clientReportRadio = document.getElementById('clientReport');
+    const allReportRadio = document.getElementById('allReport');
+
+    // Selectors
     const flightSelector = document.getElementById('flightSelector');
     const monthlySelector = document.getElementById('monthlySelector');
     const weeklySelector = document.getElementById('weeklySelector');
+    const agentSelector = document.getElementById('agentSelector');
+    const clientSelector = document.getElementById('clientSelector');
 
+    // Data Table and Download Report
+    const dataTable = document.getElementById('dataTable');
+    const downloadReport = document.getElementById('downloadReport');
+
+    // Handle Report Type Change
     flightRadio.addEventListener('change', () => 
     {
       if (flightRadio.checked) 
@@ -152,8 +228,8 @@ error_reporting(E_ALL);
         flightSelector.style.display = 'block';
         monthlySelector.style.display = 'none';
         weeklySelector.style.display = 'none';
-        document.getElementById('dataTable').style.display = 'none';
-        document.getElementById('downloadReport').style.display = 'none';
+        dataTable.style.display = 'none';
+        downloadReport.style.display = 'none';
       }
     });
 
@@ -164,8 +240,8 @@ error_reporting(E_ALL);
         monthlySelector.style.display = 'block';
         flightSelector.style.display = 'none';
         weeklySelector.style.display = 'none';
-        document.getElementById('dataTable').style.display = 'none';
-        document.getElementById('downloadReport').style.display = 'none';
+        dataTable.style.display = 'none';
+        downloadReport.style.display = 'none';
       }
     });
 
@@ -176,10 +252,76 @@ error_reporting(E_ALL);
         weeklySelector.style.display = 'block';
         flightSelector.style.display = 'none';
         monthlySelector.style.display = 'none';
-        document.getElementById('dataTable').style.display = 'none';
-        document.getElementById('downloadReport').style.display = 'none';
+        dataTable.style.display = 'none';
+        downloadReport.style.display = 'none';
       }
     });
+
+    // Handle Report For Change
+    selfReportRadio.addEventListener('change', () => 
+    {
+      if (selfReportRadio.checked) 
+      {
+        agentSelector.style.display = 'none';
+        clientSelector.style.display = 'none';
+      }
+    });
+
+    agentReportRadio.addEventListener('change', () => 
+    {
+      if (agentReportRadio.checked) 
+      {
+        agentSelector.style.display = 'block';
+        clientSelector.style.display = 'none';
+      }
+    });
+
+    clientReportRadio.addEventListener('change', () => 
+    {
+      if (clientReportRadio.checked) 
+      {
+        clientSelector.style.display = 'block';
+        agentSelector.style.display = 'none';
+      }
+    });
+
+    // Initial Setup - Trigger change events on page load to set the initial state
+    (function initialSetup() 
+    {
+      if (flightRadio.checked) 
+      {
+        flightSelector.style.display = 'block';
+      } 
+      else if (monthlyRadio.checked) 
+      {
+        monthlySelector.style.display = 'block';
+      } 
+      else if (weeklyRadio.checked) 
+      {
+        weeklySelector.style.display = 'block';
+      }
+
+      if (selfReportRadio.checked) 
+      {
+        agentSelector.style.display = 'none';
+        clientSelector.style.display = 'none';
+      } 
+      else if (agentReportRadio.checked) 
+      {
+        agentSelector.style.display = 'block';
+        clientSelector.style.display = 'none';
+      } 
+      else if (clientReportRadio.checked) 
+      {
+        clientSelector.style.display = 'block';
+        agentSelector.style.display = 'none';
+      }
+      else if (allReportRadio.checked)
+      {
+        agentSelector.style.display = 'none';
+        clientSelector.style.display = 'none';
+      }
+    })();
   </script>
 
   <!-- Script for Populating Weekly -->
