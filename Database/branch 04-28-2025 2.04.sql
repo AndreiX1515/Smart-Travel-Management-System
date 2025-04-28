954ccf01a0ff1ca0e51a9bc1c2b1b721ad898150
Vlar.sanity <cordova.clarence09@gmail.com>
2025-04-28 14:04:53 +0800

    updated db

---
+++ Database/branch 04-28-2025 2.04.sql
@@ -0,0 +1,91 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Apr 28, 2025 at 08:02 AM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `testing`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `branch`
+--
+
+CREATE TABLE `branch` (
+  `branchId` int(11) NOT NULL,
+  `companyId` int(11) DEFAULT NULL,
+  `branchName` varchar(100) DEFAULT NULL,
+  `branchLocation` varchar(100) DEFAULT NULL,
+  `branchAdd` text DEFAULT NULL,
+  `branchContactP` varchar(50) DEFAULT NULL,
+  `branchAgentCode` varchar(50) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `branch`
+--
+
+INSERT INTO `branch` (`branchId`, `companyId`, `branchName`, `branchLocation`, `branchAdd`, `branchContactP`, `branchAgentCode`) VALUES
+(1, 4, 'Branch 1', NULL, NULL, NULL, 'BU1'),
+(2, 1, 'Branch 2', NULL, NULL, NULL, 'BU2'),
+(3, 2, 'Branch 3', NULL, NULL, NULL, 'BU3'),
+(4, 5, 'E Winner', NULL, NULL, NULL, 'BU4'),
+(5, 6, 'FRANCIA', NULL, NULL, 'BU1-A003', 'BU5'),
+(6, 8, 'ESCAPE', NULL, NULL, NULL, 'BU6'),
+(7, 7, 'APD', NULL, NULL, 'BU7-A001', 'BU7'),
+(8, 3, 'Branch 9', NULL, NULL, NULL, 'BU9');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `branch`
+--
+ALTER TABLE `branch`
+  ADD PRIMARY KEY (`branchId`),
+  ADD UNIQUE KEY `branchAgentCode` (`branchAgentCode`),
+  ADD KEY `branchContactP` (`branchContactP`),
+  ADD KEY `branchCompanyId` (`companyId`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `branch`
+--
+ALTER TABLE `branch`
+  MODIFY `branchId` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `branch`
+--
+ALTER TABLE `branch`
+  ADD CONSTRAINT `branchCompanyId` FOREIGN KEY (`companyId`) REFERENCES `company` (`companyId`) ON DELETE NO ACTION ON UPDATE CASCADE,
+  ADD CONSTRAINT `branchContactP` FOREIGN KEY (`branchContactP`) REFERENCES `agent` (`agentId`) ON DELETE NO ACTION ON UPDATE CASCADE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
