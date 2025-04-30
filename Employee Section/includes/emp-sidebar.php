50ba828301a434cf9668add12e3d2db8b14c1d08
Vlar.sanity <cordova.clarence09@gmail.com>
2025-04-30 15:04:37 +0800

    function/ emp rooming list

--- Employee Section/includes/emp-sidebar.php
+++ Employee Section/includes/emp-sidebar.php
@@ -86,6 +86,17 @@ $position = htmlspecialchars(strtoupper($empId));
 		</li>
 
 		<li class="nav-item transaction">
+			<a class="nav-link page-button" href="../Employee Section/emp-roomingList.php" data-page-name="Rooming List">
+				<div class="icon-wrapper">
+					<div class="icon"><i class="fas fa-user"></i></div>
+				</div>
+				<div class="label-wrapper">
+					<span class="label">Rooming List</span>
+				</div>
+			</a>
+		</li>
+
+		<li class="nav-item transaction">
 			<a class="nav-link page-button" href="../Employee Section/emp-guestList.php" data-page-name="Guest List">
 				<div class="icon-wrapper">
 					<div class="icon"><i class="fas fa-user"></i></div>
