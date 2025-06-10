ea5dab9b20b0b15c373b76ad4bbc8fc69f91221f
AndreiX1515 <dreideguzman333@gmail.com>
2025-06-10 16:21:58 +0800

    agent fit adjustment

--- Agent Section/agent-FIT-table.php
+++ Agent Section/agent-FIT-table.php
@@ -18,11 +18,36 @@ require "../conn.php";
 </head>
 <body>
 
-<div class="body-container">
+
   <?php include "../Agent Section/includes/sidebar.php"; ?>
 
-  <div class="main-content-container">
-    <?php include "../Agent Section/includes/navbar.php"; ?>
+  <div class="main-container">
+
+    <div class="navbar">
+      <div class="page-header-wrapper">
+
+        <div class="page-header-top">
+          <div class="back-btn-wrapper">
+            <button class="back-btn" id="redirect-btn">
+              <i class="fas fa-chevron-left"></i>
+            </button>
+          </div>
+        </div>
+
+        <div class="page-header-content">
+          <div class="page-header-text">
+            <h5 class="header-title">FIT Table</h5>
+          </div>
+        </div>
+
+      </div>
+    </div>
+
+    <script>
+      document.getElementById('redirect-btn').addEventListener('click', function () {
+        window.location.href = '../Employee Section/emp-dashboard.php';
+      });
+    </script>
 
     <div class="main-content">
       <div class="table-wrapper">
@@ -222,7 +247,7 @@ require "../conn.php";
     </div>
   </div>
 
-</div>
+
 
 
 <?php require "../Agent Section/includes/scripts.php"; ?>
